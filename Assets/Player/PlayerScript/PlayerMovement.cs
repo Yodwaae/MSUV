e9f6f65e640686bb3b07080822ab2da149ec75cf
Yodwaae <lucas.spitz.drevet@gmail.com>
2022-11-04 13:26:35 +0100

    Created new tilePalette + halved grid size to fit tilePalette + modified playerMovements script to fit new grid (halved all values)
    Move on X axis are as follow : (Xsign*.5, -Xaxis*.25, 0)
    Move on Y axis are as follow : (Ysign*.5, Ysign*.25, 0)
    Offset to (0,.25,0)

--- Assets/Player/PlayerScript/PlayerMovement.cs
+++ Assets/Player/PlayerScript/PlayerMovement.cs
@@ -23,30 +23,31 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Move the player to target point (Added offset since movePoint is not at player origins (.5,.5,0))
-        transform.position = Vector3.MoveTowards(transform.position, movePoint.position - new Vector3(0, .5f, 0), moveSpeed * Time.deltaTime);
+        //Move the player to target point (Added offset since movePoint is not at player origins (0,.25,0))
+        transform.position = Vector3.MoveTowards(transform.position, movePoint.position - new Vector3(0, .25f, 0), moveSpeed * Time.deltaTime);
 
+        //All value are halved for the Y axis because of the isometric grid 
         //Prevent the Player and movePoint to registrer movement input if the player is not yet at the movePoint
-        if (Vector3.Distance(transform.position, movePoint.position - new Vector3(0, .5f, 0)) <= .02f)//Added offset since movePoint is not at player origins (.5,.5,0)
+        if (Vector3.Distance(transform.position, movePoint.position - new Vector3(0, .25f, 0)) <= .02f)//Added offset since movePoint is not at player origins (.5,.5,0)
         {
 
             if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f)//Check for X axis input
             {
                 //Checks for collision on X axis
-                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(Mathf.Sign(Input.GetAxisRaw("Horizontal")) * 1, -Mathf.Sign(Input.GetAxisRaw("Horizontal")) * .5f, 0), .2f, obstacles))
+                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(Mathf.Sign(Input.GetAxisRaw("Horizontal")) * .5f, -Mathf.Sign(Input.GetAxisRaw("Horizontal")) * .25f, 0), .2f, obstacles))
                 {
 
-                        movePoint.position += new Vector3(Mathf.Sign(Input.GetAxisRaw("Horizontal"))*1, -Mathf.Sign(Input.GetAxisRaw("Horizontal"))*.5f, 0);
+                        movePoint.position += new Vector3(Mathf.Sign(Input.GetAxisRaw("Horizontal"))*.5f, -Mathf.Sign(Input.GetAxisRaw("Horizontal"))*.25f, 0);
 
                 }
             }
             else if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f)//Check for Y axis input
             {
                 //Checks for collision on Y axis
-                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(Mathf.Sign(Input.GetAxisRaw("Vertical"))*1, Mathf.Sign(Input.GetAxisRaw("Vertical"))*.5f, 0), .2f, obstacles))
+                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(Mathf.Sign(Input.GetAxisRaw("Vertical"))*.5f, Mathf.Sign(Input.GetAxisRaw("Vertical"))*.25f, 0), .2f, obstacles))
                 {
                     
-                    movePoint.position += new Vector3(Mathf.Sign(Input.GetAxisRaw("Vertical")) * 1, Mathf.Sign(Input.GetAxisRaw("Vertical")) * .5f, 0);
+                    movePoint.position += new Vector3(Mathf.Sign(Input.GetAxisRaw("Vertical")) * .5f, Mathf.Sign(Input.GetAxisRaw("Vertical")) * .25f, 0);
 
                 }
             }
