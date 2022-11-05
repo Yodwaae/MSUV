9f09e44af86f7de3c04b14947c69a20288a68674
Yodwaae <lucas.spitz.drevet@gmail.com>
2022-11-05 14:13:02 +0100

    Added collision (and changed scene accordingly) + modified player layer + modified collider offset
    Added collision with tilemap and composite collider.
    For now collider are on the different tilemap (wall, water, ...) though I think in the end I will just create a collider tilemap to make things way easier (cause in that case I only need a -.125 offset on the Y axis and not different offsets on different tilemaps).
    
    Concerning colliders, here's what has changed :
    - collider need to be offset by -.125 on the Y axis to actually fit the grid properly, and obstacles like water need to be offset by .5 on the Y axis (so in fine a .375 offset) so that the collision is with the "top" of the tile and not the "bottom".
    - tile collider type set to "grid" from "tile"
    - once again halved value for the collision check, so the check happen with the border of the tile (where the collider actually is) and not with the center of the tile (wich allowed the player to go through obastcle) [It's a difference due to the change from normal to isometric grid]
    
    Also moved the player layer to above the wall layer, so that he would correctly be displayed behind walls.  (and tweaked the rotation on Y axis from 0 to 25 so the player sprite would better fit the isometric grid).

--- Assets/Player/PlayerScript/PlayerMovement.cs
+++ Assets/Player/PlayerScript/PlayerMovement.cs
@@ -28,13 +28,13 @@ public class PlayerMovement : MonoBehaviour
 
         //All value are halved for the Y axis because of the isometric grid 
         //Prevent the Player and movePoint to registrer movement input if the player is not yet at the movePoint
-        if (Vector3.Distance(transform.position, movePoint.position - new Vector3(0, .25f, 0)) <= .02f)//Added offset since movePoint is not at player origins (.5,.5,0)
+        if (Vector3.Distance(transform.position, movePoint.position - new Vector3(0, .25f, 0)) <= .02f)//Added offset since movePoint is not at player origins (0,.25,0)
         {
 
             if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f)//Check for X axis input
             {
                 //Checks for collision on X axis
-                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(Mathf.Sign(Input.GetAxisRaw("Horizontal")) * .5f, -Mathf.Sign(Input.GetAxisRaw("Horizontal")) * .25f, 0), .2f, obstacles))
+                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(Mathf.Sign(Input.GetAxisRaw("Horizontal")) * .25f, -Mathf.Sign(Input.GetAxisRaw("Horizontal")) * .125f, 0), .2f, obstacles))
                 {
 
                         movePoint.position += new Vector3(Mathf.Sign(Input.GetAxisRaw("Horizontal"))*.5f, -Mathf.Sign(Input.GetAxisRaw("Horizontal"))*.25f, 0);
@@ -44,7 +44,7 @@ public class PlayerMovement : MonoBehaviour
             else if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f)//Check for Y axis input
             {
                 //Checks for collision on Y axis
-                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(Mathf.Sign(Input.GetAxisRaw("Vertical"))*.5f, Mathf.Sign(Input.GetAxisRaw("Vertical"))*.25f, 0), .2f, obstacles))
+                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(Mathf.Sign(Input.GetAxisRaw("Vertical"))*.25f, Mathf.Sign(Input.GetAxisRaw("Vertical"))*.125f, 0), .2f, obstacles))
                 {
                     
                     movePoint.position += new Vector3(Mathf.Sign(Input.GetAxisRaw("Vertical")) * .5f, Mathf.Sign(Input.GetAxisRaw("Vertical")) * .25f, 0);
